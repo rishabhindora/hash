e246f1a63a2ffe3698a9d210a67cabe53085665f
Shubham Jain <shbhm89300@gmail.com>
2022-03-11 23:54:12 +0530

    created navbar toggle for smaller screen

--- javascript/indexjs.js
+++ javascript/indexjs.js
@@ -1,26 +1,18 @@
-/*const navSlide = () =>
-{
-    const logo = document.querySelector('.logo');
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navlinks');
-
-   burger.addEventListner('click', function () {
-           nav.classList.toggle('nav-active');
-       });
-    console.log('abc');
+function navSlide(){
+    navbar = document.getElementById("navbar").style
+    navbar.display = "flex"
+    navbar.flexDirection = "column"
+    navbar.zIndex = 1
+    navbar.position = "fixed"
+    navbar.top = navbar.left = 0
+    navbar.backgroundColor = "rgb(0,0,0)"
+    navbar.backgroundColor = "rgba(0,0,0, 0.9)"
+    navbar.overflowX = "hidden"
+    navbar.transition = "0.5s"
+    navbar.width = "100%"
+    navbar.height = "100vh"
+    navbar.alignItems = "center"
 }
-
-navSlide();
-*/
-/*function navSlide() {
-    const nav = document.querySelector('.navlinks');
-    nav.classList.toggle('nav-active');
-};*/
-
-function navSlide() {
-    const nav = document.querySelector('.navlinks');
-    nav.classList.toggle('nav-active');
-};
-
-
-document.getElementsByClassName("burger").addEventListener("click", navSlide);
+function closeNav(){
+    document.getElementById("navbar").style.display = "none";
+}
